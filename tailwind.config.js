75544122cd6a8b8a841fed5bb812179b8028f25e
BÜLENT İLHAN ACAR <bulentilhanacar@gmail.com>
2026-01-15 12:29:29 +0000

    https://teknikofistakip-fuigloh97-bulents-projects-9c85c37e.vercel.app/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-import type {Config} from 'tailwindcss';
 const {fontFamily} = require("tailwindcss/defaultTheme")
 
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -97,4 +97,4 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
